b1f9de34477eef0dc55232a18a2bc1686772abbf
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-05 21:59:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,16 @@
 # Copyright permission under MIT License
 # All rights reserved by FayasNoushad
 # License -> https://github.com/FayasNoushad/YouTube-Search-Bot/blob/main/LICENSE
+
+from pyrogram import Client
+
+
+Bot = Client(
+    "YouTube-Search-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+Bot.run()
