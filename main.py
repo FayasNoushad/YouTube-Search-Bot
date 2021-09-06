dc27dfd874468fac6b301cb03a96d8132a8b4e7d
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-06 11:30:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 # License -> https://github.com/FayasNoushad/YouTube-Search-Bot/blob/main/LICENSE
 
 import requests
-from pyrogram import Client
-
+from pyrogram import Client, filters
+from pyrogram.types import *
 
 Bot = Client(
     "YouTube-Search-Bot",
@@ -14,6 +14,23 @@ Bot = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+
+
+@Bot.on_message(filters.all)
+async def text(bot, update):
+    text = "Search youtube videos using below buttons.\nMade by @FayasNoushad"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(text="Search here", switch_inline_query_current_chat)],
+            [InlineKeyboardButton(text="Search in another chat", switch_inline_query)]
+        ]
+    )
+    await update.reply_text(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        quote=True
+    )
 
 
 Bot.run()
