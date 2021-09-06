84908d63f89c3deff8c1d249382ada756fd9128a
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-09-06 12:23:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ Bot = Client(
 
 @Bot.on_message(filters.all)
 async def text(bot, update):
-    text = "Search youtube videos using below buttons.\nMade by @FayasNoushad"
+    text = "Search youtube videos using below buttons.\n\nMade by @FayasNoushad"
     reply_markup = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton(text="Search here", switch_inline_query_current_chat="")],
